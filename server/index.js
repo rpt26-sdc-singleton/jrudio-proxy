8f28ae9e116b34c933d945807b9e20280c96a5f6
Shane Sutro <shanesutro@gmail.com>
2021-04-07 21:41:28 -0600

    Finish explore button and icon

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-// app.get('/:id', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
-// });
+app.get('/:id', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Proxy listening at port ${PORT}`);
