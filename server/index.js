6fa31fd3e64543006a16a8eb4cb25929460c83da
jrudio <justinjrudio@gmail.com>
2021-06-16 18:57:38 -0700

    pull service urls from .env file

--- server/index.js
+++ server/index.js
@@ -1,21 +1,38 @@
 // eslint-disable-next-line global-require
 require('newrelic');
+require('dotenv').config();
 const express = require('express');
+const fs = require('fs');
+const Mustache = require('mustache');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  // eslint-disable-line global-require
-  require('dotenv').config();
+function loadIndexWithEnv(indexPath = '') {
+  const index = fs.readFileSync(indexPath, 'utf-8');
+
+  return Mustache.render(index, {
+    titleURL: process.env.TITLE_URL || 'http://localhost:3001/bundle.js',
+    aboutURL: process.env.ABOUT_URL || 'http://localhost:3002/bundle.js',
+    reviewURL: process.env.REVIEWS_URL || 'http://localhost:3007/bundle.js',
+  });
 }
 
+const indexPath = path.resolve(__dirname, '..', 'public', 'index.html');
+
+const indexHTML = loadIndexWithEnv(indexPath);
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send(indexHTML);
+});
+
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.get('/:id', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+  res.send(indexHTML);
 });
 
 app.listen(PORT, () => {
